41abbf1ae56e4e875f8b189d14ca75d1e8e3ad27
Gustav Mattsén <mattsen03@gmail.com>
2025-11-10 02:44:46 +0100

    fixed issue with spacing

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,27 @@
 import React, { useState } from "react";
-import Sidebar from "./components/Sidebar";
-import Header from "./components/Header";
-import TaskForm from "./components/TaskForm";
-import TaskList from "./components/TaskList";
+import Sidebar from "./components/Sidebar.jsx";
+import Header from "./components/Header.jsx";
+import TaskForm from "./components/TaskForm.jsx";
+import TaskList from "./components/TaskList.jsx";
 
 function App() {
   const [tasks, setTasks] = useState([]);
 
-  const addTask = (task) => {
-    setTasks([...tasks, task]);
-  };
-
-  const deleteTask = (index) => {
-    const newTasks = tasks.filter((_, i) => i !== index);
-    setTasks(newTasks);
-  };
+  const addTask = (task) => setTasks([...tasks, task]);
+  const deleteTask = (index) => setTasks(tasks.filter((_, i) => i !== index));
 
   return (
     <div className="d-flex">
+      {/* Sidebar */}
       <Sidebar />
-      <div className="flex-grow-1 p-4">
+
+      {/* Main content */}
+      <div className="flex-grow-1 ms-5" style={{ marginLeft: "220px" }}>
         <Header />
-        <TaskForm addTask={addTask} />
-        <TaskList tasks={tasks} deleteTask={deleteTask} />
+        <div className="container mt-5 pt-5">
+          <TaskForm addTask={addTask} />
+          <TaskList tasks={tasks} deleteTask={deleteTask} />
+        </div>
       </div>
     </div>
   );
